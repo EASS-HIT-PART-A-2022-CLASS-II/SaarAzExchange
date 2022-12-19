6ed2998108559a5ac94a377ad7621b7404e59993
saar2666 <saar2666@gmail.com>
2022-12-19 22:18:31 +0200

    7

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from typing import List
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI(title="Exchange Rate API")
-db = []
 
 
 class Currency(BaseModel):
     name: str
     value: float
+
+
+class DB(BaseModel):
+    data: List[Currency]
+
+
+db = DB(data=[])
 
 
 @app.get("/")
@@ -17,21 +25,29 @@ def index():
 
 @app.post('/currency')
 def create_currency(currency: Currency):  # create currency object
-    db.append(currency.dict())  # convert it to dict
-    return db[-1]  # return last object from db list
+    db.data.append(currency)
+    return currency.dict()  # return last object from db list
 
 
 @app.get('/currency/{currency_id}')
-def get_currency_by_id(currency_id: int):
-    return db[currency_id - 1]
+def get_currency_by_id(currency_id: str):
+    for key in db.data:
+        for k, v in key:
+            if k == "name" and v == currency_id:
+                return key.dict()
+    return {"error": "no data"}
 
 
 @app.get('/currency/')
 def get_all_currency():
-    return db
+    return db.dict()
 
 
 @app.delete('/currency/{currency_id}')
-def delete_currency(currency_id: int):
-    db.pop(currency_id - 1)
-    return {}
+def delete_currency(currency_id: str):
+    for index, key, in enumerate(db.data):
+        for k, v in key:
+            if k == "name" and v == currency_id:
+                db.data.pop(index)
+                return {"deleted": "successfully"}
+    return {"error": "no data"}
--- unit_tests.py
+++ unit_tests.py
@@ -1,41 +1,41 @@
-import json
-
 from fastapi.testclient import TestClient
+from pydantic import BaseModel
 
 from main import app
 
 client = TestClient(app)
 
-data = {
-    "name": "$",
-    "value": 3.58
-}
+
+class Data(BaseModel):
+    name: str
+    value: float
+
+
+data = Data(name="$", value=3.58)
 
 
 def test_create_currency():
-    response = client.post('/currency', json=data)
+    response = client.post('/currency', json=data.dict())
     assert response.status_code == 200
     assert response.json() == data
 
 
 def test_get_all_currency():
+    # response = client.post('/currency', json=data)
+    # assert response.status_code == 200
+    global data
     response = client.get('/currency/')
-    print(response.json())
     assert response.status_code == 200
-    assert data in response.json()
+    assert data in response.json()["data"]
 
 
 def test_get_currency_by_id():
-    response = client.get('/currency/{currency_id}', json=data)
+    response = client.get('/currency/$')
     assert response.status_code == 200
     assert response.json() == data
-    print(response.json())
 
 
 def test_delete_currency():
-    response = client.delete('/currency/{currency_id}')
+    response = client.delete('/currency/$')
     assert response.status_code == 200
-    assert response.json() == {
-        "name": "Test",
-        "value": 1
-    }
+    assert response.json() == {"deleted": "successfully"}
