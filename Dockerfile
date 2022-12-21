13cf2434721b87fe6245d6ac2de5e351f8cc9c76
saar2666 <saar2666@gmail.com>
2022-12-21 17:28:21 +0200

    8

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ RUN pip install FastAPI
 RUN pip install "uvicorn[standard]"
 RUN pip install requests
 
+
+
 WORKDIR /app
 COPY . .
 
---
+++ frontend/ui.py
@@ -0,0 +1,62 @@
+# # This app is for educational purpose only. Insights gained is not financial advice. Use at your own risk!
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import requests
+import json
+
+# https://www.webfx.com/tools/emoji-cheat-sheet/
+
+# ---------------------------------#
+# New feature (make sure to upgrade your streamlit library)
+# pip install --upgrade streamlit
+
+# ---------------------------------#
+# Title
+
+image = Image.open('logo.png')
+
+st.image(image, width=500)
+
+st.title('Saar Currency Converter App')
+st.markdown("""Check how much your MONEY worth!""")
+
+# ---------------------------------#
+# Sidebar + Main panel
+st.sidebar.header('Input Options')
+
+## Sidebar - Currency price unit
+currency_list = ['USD', 'ILS', 'GBP', 'EUR']
+
+value = st.sidebar.number_input('Insert amount of currency')
+f_value = float(value)
+base_price_unit = st.sidebar.selectbox('Select base currency for conversion (FROM)', currency_list)
+symbols_price_unit = st.sidebar.selectbox('Select target currency to convert to (TO)', currency_list)
+
+
+@st.cache
+def load_data():
+    data = json.load(open('all_currency_%s.json' % base_price_unit, "r"))
+    base_currency = pd.Series(data['base_code'], name='FROM')
+    to_currency = pd.Series(data['conversion_rates'], name='TO')
+    specific_rate =pd.Series(to_currency[symbols_price_unit], name='RATE')
+    f_specific_rate = float (specific_rate)
+    final_value = (f_value*f_specific_rate)
+    target_currency = pd.Series(symbols_price_unit, name='TO')
+    conversion_date = pd.Series(data['time_last_update_utc'], name='DATE')
+    sum_exchange = pd.Series(final_value, name='TOTAL')
+    df = pd.concat([base_currency, target_currency, specific_rate,sum_exchange,conversion_date], axis=1)
+    df.style.format()
+    return df
+
+
+df = load_data()
+
+st.header('Currency conversion')
+
+st.write(df)
+
+# st.write( df.transpose() )
+
+
+# ---------------------------------#
