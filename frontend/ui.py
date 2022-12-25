a96c5a229d9ff75db51cd39f2939717172f74f7f
saar2666 <saar2666@gmail.com>
2022-12-25 15:43:05 +0200

    13

--- frontend/ui.py
+++ frontend/ui.py
@@ -15,7 +15,6 @@ backend = "http://backend:8080/"
 # Sidebar + Main panel
 st.sidebar.header('Input Options')
 
-
 currency_list = ['USD', 'ILS', 'GBP', 'EUR']
 
 value = st.sidebar.number_input('Insert amount of currency')
@@ -26,16 +25,16 @@ symbols_price_unit = st.sidebar.selectbox('Select target currency to convert to 
 
 @st.cache
 def load_data():
-    data =  requests.get(f"{backend}currency/{base_price_unit}").json()
+    data = requests.get(f"{backend}currency/{base_price_unit}").json()
     base_currency = pd.Series(base_price_unit, name='FROM')
     to_currency = pd.Series(data['conversion_rates'], name='TO')
-    specific_rate =pd.Series(to_currency[symbols_price_unit], name='RATE')
-    f_specific_rate = float (specific_rate)
-    final_value = (f_value*f_specific_rate)
+    specific_rate = pd.Series(to_currency[symbols_price_unit], name='RATE')
+    f_specific_rate = float(specific_rate)
+    final_value = (f_value * f_specific_rate)
     target_currency = pd.Series(symbols_price_unit, name='TO')
     conversion_date = pd.Series(data['time_last_update_utc'], name='DATE')
     sum_exchange = pd.Series(final_value, name='TOTAL')
-    df = pd.concat([base_currency, target_currency, specific_rate,sum_exchange,conversion_date], axis=1)
+    df = pd.concat([base_currency, target_currency, specific_rate, sum_exchange, conversion_date], axis=1)
     df.style.format()
     return df
 
